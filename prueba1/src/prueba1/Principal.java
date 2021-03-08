f986547875d03d8bfd861a58976dce893c6396a3
Andreu Bustos <andreuba13@gmail.com>
2021-03-08 16:22:16 +0100

    segundo comit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -6,7 +6,10 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main (String[]args) {
-		System.out.println("Hola mun do java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo2 java-git");
+		System.out.println("Hola mundo3 java-git");
+
 	}
 
 }
